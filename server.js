cbd6fb46fe7a837dcb1f03dd90df52898364c412
dumida <dsa@exilesoft.local>
2016-09-18 11:29:34 +0530

    change2

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 var express = require('express'),
-    wine = require('./routes/wines');
+    wines = require('./routes/wines');
 
 var app = express();
 
-app.configure(function () {
-    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser());
-});
-
-app.get('/wines', wine.findAll);
-app.get('/wines/:id', wine.findById);
-app.post('/wines', wine.addWine);
-app.put('/wines/:id', wine.updateWine);
-app.delete('/wines/:id', wine.deleteWine);
+app.get('/wines', wines.findAll);
+app.get('/wines/:id', wines.findById);
 
 app.listen(3000);
 console.log('Listening on port 3000...');
